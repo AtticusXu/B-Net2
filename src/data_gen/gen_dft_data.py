f566e89486c6d6c6f9933e854ec73bb7e8f8e7bb
Zhongshu Xu <47405148+AtticusXu@users.noreply.github.com>
2019-08-24 16:09:21 -0400

    Update gen_dft_data.py

--- src/data_gen/gen_dft_data.py
+++ src/data_gen/gen_dft_data.py
@@ -31,6 +31,40 @@ def gen_uni_data(freqmag,freqidx,siz,sig):
     ynorm =np.squeeze(np.linalg.norm(ydata,2,1))
     return xdata,ydata,ynorm
 
+def gen_ede_uni_data(freqmag,freqidx,siz,sig):
+    N = len(freqmag)
+    K = len(freqidx)
+    a = 6*np.sqrt(math.pi)*sig/math.erf(K/sig)
+
+    freqmag = np.tile(np.reshape(freqmag,[1,N]),(siz,1))
+    consty = np.random.uniform(-np.sqrt(a),np.sqrt(a),[siz,1])
+    zeroy = np.zeros([siz,1])
+    if N % 2 == 0:
+        halfy = np.random.uniform(-np.sqrt(a/2),np.sqrt(a/2),[siz,N//2-1])
+        realy = np.concatenate((consty,halfy,zeroy,halfy[:,::-1]),axis=1)
+        halfy = np.random.uniform(-np.sqrt(a/2),np.sqrt(a/2),[siz,N//2-1])
+        imagy = np.concatenate((zeroy,halfy,zeroy,-halfy[:,::-1]),axis=1)
+    else:
+        halfy = np.random.uniform(-np.sqrt(a/2),np.sqrt(a/2),[siz,N//2])
+        realy = np.concatenate((consty,halfy,halfy[:,::-1]),axis=1)
+        halfy = np.random.uniform(-np.sqrt(a/2),np.sqrt(a/2),[siz,N//2])
+        imagy = np.concatenate((zeroy,halfy,-halfy[:,::-1]),axis=1)
+
+    realy = realy*freqmag
+    imagy = imagy*freqmag
+    y = realy + imagy*1j
+    xdata = np.reshape(np.fft.ifft(y,N,1).real,(siz,N,1),order='F')
+    x_data = np.reshape(xdata,(siz,N))
+    realy = np.reshape(realy[:,freqidx],(siz,1,-1),order='F')
+    imagy = np.reshape(imagy[:,freqidx],(siz,1,-1),order='F')
+    ydata = np.reshape(np.concatenate((realy,imagy),axis=1),(siz,-1,1),order='F')
+    xdata = np.float32(xdata)
+    x_data = np.float32(x_data)
+    ydata = np.float32(ydata)
+    ynorm =np.squeeze(np.linalg.norm(ydata,2,1))
+    return xdata,ydata,ynorm,x_data
+
+    
 def gen_2D_straight_data(siz_x,siz_y,siz_u,siz_v, N):
     
     xdata = np.zeros([N,siz_x,siz_y,1])
@@ -69,7 +103,7 @@ def gen_2D_gaussian_data(freqmag,out_siz,siz,sig):
     ydata = np.float32(ydata)
     return xdata,ydata
 
-    
+
 def gen_degree_data(in_size,in_range,out_size,out_range):
     xdata = np.zeros([in_size,in_size,1])
     ydata = np.zeros([in_size,out_size,1])
@@ -115,6 +149,7 @@ def gen_energy_data(N,siz):
     DW = np.fft.fftfreq(N)
     DW[DW==0] = np.inf
     DW = 1/DW
+    print(DW)
     tmp = np.random.normal(0,1,[siz,N//8])
     xdata = np.fft.irfft(np.fft.rfft(tmp,axis=1),N,1)
     ydata = np.sum(np.absolute(np.multiply(
