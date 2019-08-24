a350c49e66332b9bfdffe1f6b032c58d9f9c1a3c
Zhongshu Xu <47405148+AtticusXu@users.noreply.github.com>
2019-08-24 16:10:21 -0400

    Update multi_test_CNNNet_inf_uni.py

--- test/multi_test_CNNNet_inf_uni.py
+++ test/multi_test_CNNNet_inf_uni.py
@@ -12,7 +12,6 @@ import numpy as np
 import scipy.io as spio
 import tensorflow as tf
 import json
-tf.reset_default_graph() 
 from gaussianfun import gaussianfun
 from gen_dft_data import gen_uni_data
 from CNNLayer import CNNLayer
@@ -31,12 +30,12 @@ freqidx = range(out_siz//2)
 freqmag = np.zeros((5*out_siz//2-3,N))
 for i in range(5*out_siz//2-4):
     freqmag[i] = np.fft.ifftshift(gaussianfun(np.arange(-N//2,N//2),
-                                       [-i/5],[sig]))
+                                       [i/5],[sig]))
     freqmag[i,N//2] = 0
 for i in range(out_siz//2):  
     freqmag[5*out_siz//2-4,i]=0.05
     freqmag[5*out_siz//2-4,-i]=0.05
-
+print(freqmag[3])
 #=========================================================
 #----- Parameters Setup
 
@@ -75,119 +74,85 @@ print("Prefix Coef:      %6r" % (prefixed))
 print("In Range:        (%6.2f, %6.2f)" % (in_range[0], in_range[1]))
 print("Out Range:       (%6.2f, %6.2f)" % (out_range[0], out_range[1]))
 
-
-#=========================================================
-#----- Variable Preparation
-sess = tf.Session()
-
-trainInData = tf.placeholder(tf.float32, shape=(batch_siz,in_siz,1),
-        name="trainInData")
-trainOutData = tf.placeholder(tf.float32, shape=(batch_siz,out_siz,1),
-        name="trainOutData")
-trainNorm = tf.placeholder(tf.float32, shape=(batch_siz),
-        name="trainNorm")
-testInData = tf.placeholder(tf.float32, shape=(test_batch_siz,in_siz,1),
-        name="trainInData")
-testOutData = tf.placeholder(tf.float32, shape=(test_batch_siz,out_siz,1),
-        name="trainOutData")
-testNorm = tf.placeholder(tf.float32, shape=(test_batch_siz),
-        name="testNorm")
-global_steps=tf.Variable(0, trainable=False)
-#=========================================================
-#----- Training Preparation
-CNN_net = CNNLayer(in_siz, out_siz, False, klvl, alph,
-        channel_siz, nlvl, prefixed)
-
-learning_rate = tf.train.exponential_decay(adam_learning_rate,
-                                           global_steps,100,
-                                           adam_learning_rate_decay)
-optimizer_adam = tf.train.AdamOptimizer(learning_rate,
-        adam_beta1, adam_beta2)
-
-y_train_output = CNN_net(trainInData)
-y_test_output = CNN_net(testInData)
-
-MSE_loss_train = tf.reduce_mean(
-        tf.squared_difference(trainOutData, y_train_output))
-
-L2_loss_train = tf.reduce_mean(tf.divide(tf.sqrt(tf.reduce_sum(tf.squeeze(
-        tf.squared_difference(trainOutData, y_train_output)),1)),trainNorm))
-
-Sqr_loss_train_K = tf.reduce_mean(tf.squeeze(tf.squared_difference(
-        trainOutData, y_train_output)),0)
-
-
-y_norm_train = tf.reduce_mean(trainNorm)
-
-L2_loss_test = tf.reduce_mean(tf.divide(tf.sqrt(tf.reduce_sum(tf.squeeze(
-        tf.squared_difference(testOutData, y_test_output)),1)),testNorm))
-
-L2_loss_test_list = tf.divide(tf.sqrt(tf.reduce_sum(tf.squeeze(
-        tf.squared_difference(testOutData, y_test_output)),1)),testNorm)
-
-Sqr_loss_test_K = tf.reduce_mean(tf.squeeze(
-        tf.squared_difference(testOutData, y_test_output)),0)
-
-Sqr_test_norm_K = tf.reduce_mean(tf.squeeze(tf.square(testOutData)),0)
-
-train_step = optimizer_adam.minimize(MSE_loss_train,global_step=global_steps)
-
-# Initialize Variables
-init = tf.global_variables_initializer()
-
-print("Total Num Paras:  %6d" % ( np.sum( [np.prod(v.get_shape().as_list())
-    for v in tf.trainable_variables()]) ))
-
-#=========================================================
-#----- Step by Step Training
-S=2
-K_list = np.zeros((S,out_siz,max_iter//record_freq)) 
-err_list = np.zeros((S,max_iter//record_freq))
-mk_test_loss_list = np.zeros((5*out_siz//2-3,S))
-epochs = np.linspace(0,max_iter,max_iter//record_freq)
-test_loss_klist = np.zeros((S,out_siz//2))
-for s in range(S):
-    sess.run(init)
+def train():
+    #=========================================================
+    #----- Variable Preparation
+    sess = tf.Session()
     
-    for it in range(max_iter):
-        x_train,y_train,y_norm = gen_uni_data(freqmag[-1],freqidx,batch_siz,sig)
-        train_dict = {trainInData: x_train, trainOutData: y_train,
-                  trainNorm: y_norm}
-        if it % report_freq == 0:
-            temp_train_loss = sess.run(L2_loss_train, feed_dict=train_dict)
-            print("Iter # %6d: Train Loss: %10e." % (it,temp_train_loss))
-        if it % record_freq == 0:
-            K_loss = sess.run(Sqr_loss_train_K,feed_dict=train_dict)
-            err_list[s,it//record_freq] = temp_train_loss
-            K_list[s,:,it//record_freq] = K_loss
-        sess.run(train_step, feed_dict=train_dict)
-    for j in range(5*out_siz//2-3):
-        x_test,y_test,y_norm = gen_uni_data(freqmag[j],freqidx,test_batch_siz,sig)
-        test_dict = {testInData: x_test, testOutData: y_test,
-                      testNorm: y_norm}
-        [test_loss, test_loss_list, test_loss_k,K_norm] = sess.run(
-                [L2_loss_test, L2_loss_test_list, Sqr_loss_test_K,Sqr_test_norm_K],
-                feed_dict=test_dict)
-        mk_test_loss_list[j,s] = test_loss
-    #print("Test Loss: %10e." % (test_loss))
+    trainInData = tf.placeholder(tf.float32, shape=(batch_siz,in_siz,1),
+                                 name="trainInData")
+    trainOutData = tf.placeholder(tf.float32, shape=(batch_siz,out_siz,1),
+                                  name="trainOutData")
+    trainNorm = tf.placeholder(tf.float32, shape=(batch_siz),
+                               name="trainNorm")
 
-    #for i in range(8):
-    #    K_norm[i] = np.sqrt(K_norm[2*i] + K_norm[2*i+1])
-    #    K_list[s,i,:] = np.sqrt(K_list[s,2*i,:] + K_list[s,2*i+1,:])\
-    #                   /K_norm[i]
-    #    test_loss_klist[s,i] = np.sqrt(test_loss_k[2*i] + test_loss_k[2*i+1])\
-    #                           /K_norm[i]
+    global_steps=tf.Variable(0, trainable=False)
+    #=========================================================
+    #----- Training Preparation
+    CNN_net = CNNLayer(in_siz, out_siz, False, klvl, alph,
+                       channel_siz, nlvl, prefixed)
+    
+    learning_rate = tf.train.exponential_decay(adam_learning_rate,
+                                               global_steps,100,
+                                               adam_learning_rate_decay)
+    optimizer_adam = tf.train.AdamOptimizer(learning_rate,
+                                                adam_beta1, adam_beta2)
+    
+    y_train_output = CNN_net(trainInData)
+    
+    MSE_loss_train = tf.reduce_mean(
+            tf.squared_difference(trainOutData, y_train_output))
+    
+    L2_loss_train = tf.reduce_mean(tf.divide(tf.sqrt(tf.reduce_sum(tf.squeeze(
+            tf.squared_difference(trainOutData, y_train_output)),1)),trainNorm))
+    
+    Sqr_loss_train_K = tf.reduce_mean(tf.squeeze(tf.squared_difference(
+            trainOutData, y_train_output)),0)
+    
+    
+    y_norm_train = tf.reduce_mean(trainNorm)
 
-#err_list = np.log10(err_list)
-#K_list = np.log10(K_list)
-mk_test_loss_list = np.mean(np.log10(mk_test_loss_list),axis = 1)
-#print(test_loss_klist)
-#print(K_norm[0:out_siz//2])
-print(mk_test_loss_list)
-#for k in range(out_siz//2):
-#    fig = plt.figure(k,figsize=(10,8))
-#    for s in range(S):
-#        plt.plot(epochs, K_list[s,k], 'r', label = 'k = '+ str(k)+')')
-#    plt.title('CNN_Training Error Plot(k='+ str(k)+')')
-#    plt.savefig("CNN_Train_Error_"+ str(prefixed)+"_"+str(k)+".png" )
-sess.close()
+    train_step = optimizer_adam.minimize(MSE_loss_train,global_step=global_steps)
+    
+    # Initialize Variables
+    init = tf.global_variables_initializer()
+    
+    print("Total Num Paras:  %6d" % ( np.sum( [np.prod(v.get_shape().as_list())
+    for v in tf.trainable_variables()]) ))
+    
+    #=========================================================
+    #----- Step by Step Training
+    S=2
+    K_list = np.zeros((S,out_siz,max_iter//record_freq)) 
+    err_list = np.zeros((S,max_iter//record_freq))
+    for s in range(S):
+        saver = tf.train.Saver()
+        sess.run(init)
+        MODEL_SAVE_PATH = "train_model/"
+        MODEL_NAME = "cnn_"+str(prefixed)+"_"+str(s)+"_model"
+        for it in range(max_iter):
+            x_train,y_train,y_norm = gen_uni_data(freqmag[0],freqidx,batch_siz,sig)
+            train_dict = {trainInData: x_train, trainOutData: y_train,
+                          trainNorm: y_norm}
+            if it % report_freq == 0:
+                temp_train_loss = sess.run(L2_loss_train, feed_dict=train_dict)
+                print("Iter # %6d: Train Loss: %10e." % (it,temp_train_loss))
+            if it % record_freq == 0:
+                K_loss = sess.run(Sqr_loss_train_K,feed_dict=train_dict)
+                err_list[s,it//record_freq] = temp_train_loss
+                K_list[s,:,it//record_freq] = K_loss
+            sess.run(train_step, feed_dict=train_dict)
+        
+        saver.save(sess, MODEL_SAVE_PATH + MODEL_NAME+".ckpt")
+    np.save('train_model/cnn_err_list_'+str(prefixed), err_list)
+    np.save('train_model/cnn_K_list_'+str(prefixed), K_list)  
+    sess.close()
+
+
+def main(argv=None):
+    tf.reset_default_graph()
+    train()
+
+
+if __name__ == '__main__':
+    main()
